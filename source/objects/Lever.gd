36bccb7e08a3ecaec96c96d3a9ff9f6c58e3c221
Aaron Winter <winter.aaron93@gmail.com>
2019-06-03 20:40:48 +0200

    Revert "Revert "added doors and levers and plates""
    This reverts commit 98139ff8d867fb5a9e36ba596ba8e108ef76a402.

---
+++ source/objects/Lever.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+signal activated
+signal deactivated
+
+var activated = false
+
+onready var anim := $AnimationPlayer as AnimationPlayer
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and has_player():
+		if activated:
+			anim.play_backwards("activate")
+			activated = false
+			emit_signal("deactivated")
+		else:
+			anim.play("activate")
+			activated = true
+			emit_signal("activated")
+
+func has_player() -> bool:
+	if Global.Boy:
+		return get_overlapping_bodies().has(Global.Boy)
+	return false
