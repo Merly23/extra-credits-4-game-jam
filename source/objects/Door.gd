f76712bdf74691e8c8243687738f76eefee4d342
Aaron Winter <winter.aaron93@gmail.com>
2019-06-04 00:44:59 +0200

    added toggle() to door and gate

--- source/objects/Door.gd
+++ source/objects/Door.gd
@@ -4,12 +4,22 @@ onready var anim := $AnimationPlayer as AnimationPlayer
 onready var coll := $CollisionShape2D as CollisionShape2D
 onready var ghost_block := $GhostBlock
 
+var opened := false
+
 func activate() -> void:
+	opened = true
 	anim.play("open")
 	coll.disabled = true
 	ghost_block.disabled = true
 
 func deactivate() -> void:
+	opened = false
 	anim.play_backwards("open")
 	coll.disabled = false
 	ghost_block.disabled = false
+
+func toggle():
+	if opened:
+		deactivate()
+	else:
+		activate()
--- source/objects/Gate.gd
+++ source/objects/Gate.gd
@@ -3,10 +3,20 @@ extends StaticBody2D
 onready var anim := $AnimationPlayer as AnimationPlayer
 onready var coll := $CollisionShape2D as CollisionShape2D
 
+var opened := false
+
 func activate() -> void:
+	opened = true
 	anim.play("open")
 	coll.disabled = true
 
 func deactivate() -> void:
+	opened = false
 	anim.play_backwards("open")
 	coll.disabled = false
+
+func toggle():
+	if opened:
+		deactivate()
+	else:
+		activate()
