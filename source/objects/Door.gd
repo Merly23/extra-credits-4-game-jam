fb4d2bbcae5597c788158fa8a575365bc17ce6a8
Aaron Winter <winter.aaron93@gmail.com>
2019-06-04 00:27:20 +0200

    doors block ghosts

--- source/objects/Door.gd
+++ source/objects/Door.gd
@@ -2,11 +2,14 @@ extends StaticBody2D
 
 onready var anim := $AnimationPlayer as AnimationPlayer
 onready var coll := $CollisionShape2D as CollisionShape2D
+onready var ghost_block := $GhostBlock
 
 func activate() -> void:
 	anim.play("open")
 	coll.disabled = true
+	ghost_block.disabled = true
 
 func deactivate() -> void:
 	anim.play_backwards("open")
 	coll.disabled = false
+	ghost_block.disabled = false
--- source/objects/GhostBlock.gd
+++ source/objects/GhostBlock.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var disabled = false
+
 func _on_GhostBlock_body_entered(body: PhysicsBody2D) -> void:
-	if body == Global.Girl:
+	if body == Global.Girl and not disabled:
 		body.blocked()
