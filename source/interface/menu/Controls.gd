a6150e42fdd234cb96a5276864cd385b2b0d3ccc
Aaron Winter <winter.aaron93@gmail.com>
2019-06-04 02:47:18 +0200

    button sound

--- source/interface/menu/Controls.gd
+++ source/interface/menu/Controls.gd
@@ -1,4 +1,5 @@
 extends Panel
 
 func _on_Back_pressed() -> void:
+	Audio.play_button()
 	Scene.change(Scene.TitleScreen)
--- source/interface/menu/TitleScreen.gd
+++ source/interface/menu/TitleScreen.gd
@@ -1,15 +1,20 @@
 extends Panel
+
 func _ready() -> void:
 	Audio.play_menu_music()
 
 func _on_Game_pressed() -> void:
+	Audio.play_button()
 	Scene.change(Scene.Level1, true)
 
 func _on_Controls_pressed() -> void:
+	Audio.play_button()
 	Scene.change(Scene.Controls)
 
 func _on_Credits_pressed() -> void:
+	Audio.play_button()
 	Scene.change(Scene.Credits)
 
 func _on_Exit_pressed() -> void:
+	Audio.play_button()
 	get_tree().quit()
