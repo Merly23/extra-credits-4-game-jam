962898cf6d73fd00dc871d9df3abde449b33a28e
Aaron Winter <winter.aaron93@gmail.com>
2019-06-04 21:19:39 +0200

    fixed cam to prevent wobbly objects

--- source/levels/GameCam.gd
+++ source/levels/GameCam.gd
@@ -12,5 +12,7 @@ func update_position(boy_position: Vector2, girl_position: Vector2) -> void:
 		new_position = (boy_position + girl_position) / 2
 		new_position = Vector2(floor(new_position.x), floor(new_position.y))
 
-	tween.interpolate_property(self, "global_position", global_position, new_position, 0.06, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-	tween.start()
+#	tween.interpolate_property(self, "global_position", global_position, new_position, 0.06, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+#	tween.start()
+
+	global_position = new_position
